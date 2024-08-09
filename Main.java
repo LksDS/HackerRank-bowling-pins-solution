d5a6efdf186fa7a8f5f0b08ab06297bd2e4d1c40
Lucas <121774348+LksDS@users.noreply.github.com>
2024-08-09 14:56:59 -0300

    Solution working but exceeding time

--- Main.java
+++ Main.java
@@ -3,20 +3,29 @@ import java.util.Scanner;
 
 public class Main {
     public static String isWinning(int n, String config) {
-        // Return WIN or LOSE depending on whether you will win
-        if (config.indexOf('I') == -1){
+
+        if (!config.contains("I")) {
             return "LOSE";
         }
-        if (config.indexOf('I') == config.lastIndexOf('I')){
-            return "WIN";
+
+        for (int i = 0; i < n; i++) {
+            if (config.charAt(i) == 'I') {
+
+                String newConfig = config.substring(0, i) + 'X' + config.substring(i + 1);
+                if (isWinning(n, newConfig).equals("LOSE")) {
+                    return "WIN";
+                }
+
+                if (i + 1 < n && config.charAt(i + 1) == 'I') {
+                    newConfig = config.substring(0, i) + "XX" + config.substring(i + 2);
+                    if (isWinning(n, newConfig).equals("LOSE")) {
+                        return "WIN";
+                    }
+                }
+            }
         }
-        if (config.indexOf('I')+1 < config.length() && config.charAt(config.indexOf('I')+1) == 'I'){
-            String sequencia = config.replaceFirst("II","XX");
-            return isWinning(sequencia.length(),sequencia).equals("WIN") ? "LOSE":"WIN";
-        }else{
-            String sequencia = config.replaceFirst("I","X");
-            return isWinning(sequencia.length(),sequencia).equals("WIN")? "LOSE":"WIN";
-        }
+
+        return "LOSE";
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
